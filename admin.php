5efb3ae4eea17bab7aa4a3e89ad98ea905eff4b4
julienrusev <julienrusev@gmail.com>
2018-12-18 11:20:01 +0200

    Footer styles added

--- admin.php
+++ admin.php
@@ -20,25 +20,23 @@
 	    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	</head>
 	<body>
-	<div class="header_include">
-		<div class="header_wrapper">
-			<a href="../index"><img src="../resources/media/logo.png" alt="logo" class="header_logo"></a>
-			<div class="header_menu">
-				<p class="loginHeader">LOG IN TO ADMIN PAGE</p>
+	<div id="admin">
+		<div class="header_include">
+			<div class="header_wrapper">
+				<a href="../index"><img src="../resources/media/logo.png" alt="logo" class="header_logo"></a>
+				<div class="header_menu">
+					<p class="loginHeader">LOG IN TO ADMIN PAGE</p>
+				</div>
 			</div>
 		</div>
+		<div class="login_form">
+		   <form action = "" method = "post">
+		      <label><span class="submitPage_required">*</span>UserName:</label><br>
+		      <input class="loginPage_input" type = "text" name = "username" class = "box"/ required=""><br><br>
+		      <label><span class="submitPage_required">*</span>Password:</label><br>
+		      <input class="loginPage_input" type = "password" name = "password" class = "box" / required=""><br><br>
+		      <input class="loginPage_button" type = "submit" value = " LOG IN "/><br/><br/>
+		   </form>
+		</div>
+		<?php include("include/footer.php"); ?>
 	</div>
-	<div class="login_form">
-	   <form action = "" method = "post">
-	      <label><span class="submitPage_required">*</span>UserName:</label><br>
-	      <input class="loginPage_input" type = "text" name = "username" class = "box"/ required=""><br><br>
-	      <label><span class="submitPage_required">*</span>Password:</label><br>
-	      <input class="loginPage_input" type = "password" name = "password" class = "box" / required=""><br><br>
-	      <input class="loginPage_button" type = "submit" value = " LOG IN "/><br/><br/>
-	   </form>
-	</div>
-
-
-<?php 
-   include("include/footer.php");
-?>
